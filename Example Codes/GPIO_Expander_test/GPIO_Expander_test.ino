3c2bc8ed32396b1935354f8b9cb0d1c8615fa8c5
Clarissa <41070171+cmmagsipoc16@users.noreply.github.com>
2018-08-03 10:55:33 +0800

    First commit

---
+++ Example Codes/GPIO_Expander_test/GPIO_Expander_test.ino
@@ -0,0 +1,75 @@
+#include <SPI.h>
+#include <MCP23S17.h>
+
+MCP IOchip(0,5);  // create an object at address 0 called "IOchip" with SlaveSelect at GPIO5 pin of ESP 
+                  // address of the device configured with address (pins A0, A1, A2)
+                  // In this case address 0 means to ground A0, A1, A2
+void setup() {
+  // put your setup code here, to run once:
+  // Serial.begin(115200);
+  IOchip.begin(); 
+  
+//Initialize GPIO A             //In MCP23S17
+  IOchip.pinMode(1, OUTPUT);    //GPA-0 [pin 21]
+  IOchip.pinMode(2, OUTPUT);    //GPA-1 [pin 22]
+  IOchip.pinMode(3, OUTPUT);    //GPA-2 [pin 23]
+  IOchip.pinMode(4, OUTPUT);    //GPA-3 [pin 24]
+  IOchip.pinMode(5, OUTPUT);    //GPA-4 [pin 25]
+  IOchip.pinMode(6, OUTPUT);    //GPA-5 [pin 26]
+  IOchip.pinMode(7, OUTPUT);    //GPA-6 [pin 27]
+  IOchip.pinMode(8, OUTPUT);    //GPA-7 [pin 28]
+  
+//Initialize GPIO B             //In MCP23S17
+  IOchip.pinMode(9, OUTPUT);    //GPB-0 [pin 1]
+  IOchip.pinMode(10, OUTPUT);   //GPB-1 [pin 2]
+  IOchip.pinMode(11, OUTPUT);   //GPB-2 [pin 3]
+  IOchip.pinMode(12, OUTPUT);   //GPB-3 [pin 4]
+  IOchip.pinMode(13, OUTPUT);   //GPB-4 [pin 5]
+  IOchip.pinMode(14, OUTPUT);   //GPB-5 [pin 6]
+  IOchip.pinMode(15, OUTPUT);   //GPB-4 [pin 5]
+  IOchip.pinMode(16, OUTPUT);   //GPB-5 [pin 6]  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//TURN OFF GPA
+ IOchip.digitalWrite(1, LOW); 
+ IOchip.digitalWrite(2, LOW); 
+ IOchip.digitalWrite(3, LOW); 
+ IOchip.digitalWrite(4, LOW); 
+ IOchip.digitalWrite(5, LOW); 
+ IOchip.digitalWrite(6, LOW); 
+ IOchip.digitalWrite(7, LOW); 
+ IOchip.digitalWrite(8, LOW);
+//TURN OFF GPB    
+ IOchip.digitalWrite(9, HIGH);
+ IOchip.digitalWrite(10, HIGH); 
+ IOchip.digitalWrite(11, HIGH); 
+ IOchip.digitalWrite(12, HIGH); 
+ IOchip.digitalWrite(13, HIGH);
+ IOchip.digitalWrite(14, HIGH); 
+ IOchip.digitalWrite(15, HIGH);
+ IOchip.digitalWrite(16, HIGH);  
+
+ delay(1000);
+
+//TURN ON GPA
+ IOchip.digitalWrite(1, HIGH);
+ IOchip.digitalWrite(2, HIGH); 
+ IOchip.digitalWrite(3, HIGH); 
+ IOchip.digitalWrite(4, HIGH);
+ IOchip.digitalWrite(5, HIGH); 
+ IOchip.digitalWrite(6, HIGH); 
+ IOchip.digitalWrite(7, HIGH); 
+ IOchip.digitalWrite(8, HIGH);
+//TURN ON GPB
+ IOchip.digitalWrite(9, LOW);
+ IOchip.digitalWrite(10, LOW); 
+ IOchip.digitalWrite(11, LOW); 
+ IOchip.digitalWrite(12, LOW);
+ IOchip.digitalWrite(13, LOW);
+ IOchip.digitalWrite(14, LOW); 
+ IOchip.digitalWrite(15, LOW);
+ IOchip.digitalWrite(16, LOW);
+ delay(1000); 
+}
---
+++ Example Codes/libraries/Mcp3208/README.md
@@ -0,0 +1,20 @@
+# Arduino library for MCP3208/3204 ADC
+
+This library provides an interface to communicate with the MCP3208/3204 12 bit ADC over SPI.
+
+## Hardware
+
+The library is implemented for the 8 channel (MCP3208) version but is fully
+compatible with the 4 channel (MCP3204) version.
+
+When used with the 4 channel version the available channels are limited to
+the first 4 single channels (SINGLE_0-4) and the first 2 differential
+channels (DIFF_0/1).
+
+## Documentation
+
+The documentation is available [here](https://labfruits.github.io/mcp3208/docs/html/).
+
+## License
+
+This code is released under the MIT License.
